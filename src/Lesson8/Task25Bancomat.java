20fa862b906e9f02939100c3d8d10ef73045b487
Safer68 <Safer@yandex.ru>
2021-12-05 13:01:53 +0300

    Исправил Task25Bancomat

--- src/Lesson8/Task25Bancomat.java
+++ src/Lesson8/Task25Bancomat.java
@@ -4,117 +4,114 @@ package Lesson8;
 import java.util.Scanner;
 
 public class Task25Bancomat {
+    private static final int DENOMINATION_BILLS_20 = 20;
+    private static final int DENOMINATION_BILLS_50 = 50;
+    private static final int DENOMINATION_BILLS_100 = 100;
 
-    private int naminal20;
-    private int naminal50;
-    private int naminal100;
+    private int quantityBanknote20;
+    private int quantityBanknote50;
+    private int quantityBanknote100;
     private int balance;
 
-    public Task25Bancomat(int naminal20, int naminal50, int naminal100) {
-        this.naminal20 = naminal20;
-        this.naminal50 = naminal50;
-        this.naminal100 = naminal100;
-        this.balance = naminal20 * 20 + naminal50 * 50 + naminal100 * 100;
+    public Task25Bancomat(int quantityBanknote20, int quantityBanknote50, int quantityBanknote100) {
+        this.quantityBanknote20 = quantityBanknote20;
+        this.quantityBanknote50 = quantityBanknote50;
+        this.quantityBanknote100 = quantityBanknote100;
+        this.balance = quantityBanknote20 * DENOMINATION_BILLS_20 + quantityBanknote50 * DENOMINATION_BILLS_50 + quantityBanknote100 * DENOMINATION_BILLS_100;
     }
 
-    public int getNaminal20() {
-        return naminal20;
+    public int getQuantityBanknote20() {
+        return quantityBanknote20;
     }
 
-    public void setNaminal20(int naminal20) {
-        this.naminal20 = naminal20;
+    public void setQuantityBanknote20(int quantityBanknote20) {
+        balance += quantityBanknote20 * DENOMINATION_BILLS_20;
+        this.quantityBanknote20 += quantityBanknote20;
     }
 
-    public int getNaminal50() {
-        return naminal50;
+    public int getQuantityBanknote50() {
+        return quantityBanknote50;
     }
 
-    public void setNaminal50(int naminal50) {
-        this.naminal50 = naminal50;
+    public void setQuantityBanknote50(int quantityBanknote50) {
+        balance += quantityBanknote50 * DENOMINATION_BILLS_50;
+        this.quantityBanknote50 += quantityBanknote50;
     }
 
-    public int getNaminal100() {
-        return naminal100;
+    public int getQuantityBanknote100() {
+        return quantityBanknote100;
     }
 
-    public void setNaminal100(int naminal100) {
-        this.naminal100 = naminal100;
+    public void setQuantityBanknote100(int quantityBanknote100) {
+        balance += quantityBanknote100 * DENOMINATION_BILLS_100;
+        this.quantityBanknote100 += quantityBanknote100;
     }
-
-    private int minNaminal() {
-        if (naminal20 > 0) {
-            return 20;
-        } else if (naminal50 > 0) {
-            return 50;
-        } else {
-            return 100;
-        }
-    }
-
-    private boolean writeOffBalance(int summ) {
-        int n100 = naminal100;
-        int n50 = naminal50;
-        int n20 = naminal20;
-        int summSpis = summ;
-        if (summSpis >= 100 && n100 != 0) {
-            for (int i = n100; 0 < i; i--) {
-                if (summSpis >= 100) {
-                    summSpis = summSpis - 100;
-                    n100--;
-                } else break;
-            }
-        }
-        if (summSpis >= 50 && n50 != 0) {
-            for (int i = n50; 0 < i; i--) {
-                if (summSpis >= 50) {
-                    summSpis = summSpis - 50;
-                    n50--;
-                } else break;
-            }
-        }
-        if (summSpis >= 20 && n20 != 0) {
-            for (int i = n20; 0 < i; i--) {
-                if (summSpis >= 20) {
-                    summSpis = summSpis - 20;
-                    n20--;
-                } else break;
-            }
-
-        }
-        if (summSpis == 0) {
-            System.out.println("Списано 100 купюр: " + (naminal100 - n100) + "шт.");
-            naminal100 = naminal100 - n100;
-            System.out.println("Списано 50 купюр: " + (naminal50 - n50) + "шт.");
-            naminal100 = naminal50 - n50;
-            System.out.println("Списано 20 купюр: " + (naminal20 - n20) + "шт.");
-            naminal100 = naminal50 - n20;
-            balance = balance-summ;
-            return true;
-        }else return false;
-    }
-
 
     public boolean getMoney() {
         Scanner in = new Scanner(System.in);
-        int summ = in.nextInt();
-        if (summ < balance && summ % minNaminal() == 0) {
-            return writeOffBalance(summ);
+        int writeOffAmount;
+        if (in.hasNext()) {
+            writeOffAmount = in.nextInt();
+            if (writeOffAmount < balance) {
+                return writeOffBalance(writeOffAmount);
+            } else {
+                System.out.println("Недостаточно средств!");
+                return false;
+            }
+        }
+        return false;
+    }
 
-        } else if (summ > balance && summ % minNaminal() != 0) {
-            System.out.println("Нет купюр нужного номинала!");
-            return false;
+    private boolean writeOffBalance(int writeOffAmount) {
+        int var100 = quantityBanknote100;
+        int var50 = quantityBanknote50;
+        int var20 = quantityBanknote20;
+        int varSum = writeOffAmount;
+
+        while (varSum >= DENOMINATION_BILLS_100 && var100 != 0) {
+            varSum -= DENOMINATION_BILLS_100;
+            var100--;
+        }
+
+        if (varSum % DENOMINATION_BILLS_50 == 0 || (varSum % DENOMINATION_BILLS_50) % DENOMINATION_BILLS_20 == 0) {
+            while (varSum >= DENOMINATION_BILLS_50 && var50 != 0) {
+                varSum -= DENOMINATION_BILLS_50;
+                var50--;
+            }
+        }
+
+        while (varSum >= DENOMINATION_BILLS_20 && var20 != 0) {
+            varSum -= DENOMINATION_BILLS_20;
+            var20--;
+        }
+
+        if (varSum == 0) {
+            if (var100 != quantityBanknote100) {
+                System.out.println("Выдано купюр номиналом 100: " + (quantityBanknote100 - var100) + "шт.");
+                quantityBanknote100 -= quantityBanknote100 - var100;
+            }
+            if (var50 != quantityBanknote50) {
+                System.out.println("Выдано купюр номиналом 50: " + (quantityBanknote50 - var50) + "шт.");
+                quantityBanknote50 -= quantityBanknote50 - var50;
+            }
+            if (var20 != quantityBanknote20) {
+                System.out.println("Выдано купюр номиналом 20: " + (quantityBanknote20 - var20) + "шт.");
+                quantityBanknote20 = quantityBanknote20 - (quantityBanknote20 - var20);
+            }
+            balance = balance - writeOffAmount;
+            return true;
         } else {
-            System.out.println("Недостаточно средств!");
+            System.out.println("Нет купюр нужного номинала!");
             return false;
         }
     }
 
     public static void main(String[] args) {
-        Task25Bancomat task25Bancomat = new Task25Bancomat(45,53,46);
+        Task25Bancomat task25Bancomat = new Task25Bancomat(45, 0, 46);
+        System.out.println(task25Bancomat.getMoney());
+        task25Bancomat.setQuantityBanknote50(5);
         System.out.println(task25Bancomat.getMoney());
         System.out.println(task25Bancomat.getMoney());
         System.out.println(task25Bancomat.getMoney());
-        System.out.println(task25Bancomat.getMoney());
-
     }
 }
