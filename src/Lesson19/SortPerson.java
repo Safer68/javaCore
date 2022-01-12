67a39323d6473c7a84adc541525a9289fdd8ee0a
Safer68 <Safer@yandex.ru>
2022-01-12 22:01:36 +0300

    Выполнил задание Lesson19

---
+++ src/Lesson19/SortPerson.java
@@ -0,0 +1,16 @@
+package Lesson19;
+
+import java.util.Comparator;
+
+public class SortPerson implements Comparator<Person>{
+    @Override
+    public int compare(Person o1, Person o2) {
+
+        if (o1.getSurname().compareTo(o2.getSurname())==0) {
+
+                return o1.getName().compareTo(o2.getName());
+
+           }else return o1.getSurname().compareTo(o2.getSurname());
+    }
+
+}
